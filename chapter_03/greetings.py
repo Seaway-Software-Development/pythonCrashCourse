310dec9366557ff28d5a61df927bd1c2ee2ea9e2
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2022-04-10 16:07:10 -0400

    Added greetings and guitars files.

---
+++ chapter_03/greetings.py
@@ -0,0 +1,3 @@
+names = ["Kim", "Tim", "Karen", "Terry", "Deb", "Rik"]
+for n in names:
+    print("Hello there", n , ", how are you today?")
---
+++ chapter_03/guitars.py
@@ -0,0 +1,3 @@
+guitars = ["J200", "Takamine", "Hummingbird", "Telecaster"]
+for g in guitars:
+    print("I would love to own a",g,"guitar!")
