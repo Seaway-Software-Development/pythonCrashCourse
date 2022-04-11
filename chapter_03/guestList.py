16f71f1a879d3c0710caedc7469fa4a6f1f7a4a0
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2022-04-10 21:43:23 -0400

    Added guestList.py

---
+++ chapter_03/guestList.py
@@ -0,0 +1,6 @@
+guests = ["Marilyn Monroe", "John Lennon", "Dave Grohl", "Paul McCartney", "Babe Ruth"]
+for g in guests:
+    print("Hello",g,", I would be honored if you would join me for dinner.")
+    
+print("Thank you,")
+print("David Wells")
