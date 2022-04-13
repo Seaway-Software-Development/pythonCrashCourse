6b789ac05955330cade6353cee1137a1f22218a3
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2022-04-13 08:48:52 -0400

    file_reader.py

--- chapter_10/file_reader.py
+++ chapter_10/file_reader.py
@@ -1,7 +1,3 @@
-filename = 'pi_digits.txt'
-
-with open(filename) as file_object:
-    lines = file_object.readlines()
-
-for line in lines:
-    print(line.rstrip())
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+print(contents)
