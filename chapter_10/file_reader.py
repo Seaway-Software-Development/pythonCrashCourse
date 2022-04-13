c374fbada5aa1674a4e177df53ccb69661fed1d5
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2022-04-13 13:33:56 -0400

    added files

--- chapter_10/file_reader.py
+++ chapter_10/file_reader.py
@@ -1,3 +1,5 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+file = ('/Users/david.wells/Desktop/pythonCrashCourse/chapter_10/pi_digits.txt')
+
+with open(file) as file_object:
+    lines = file_object.readlines()
+    
--- chapter_10/pi_string.py
+++ chapter_10/pi_string.py
@@ -1,11 +1,17 @@
-filename = 'pi_million_digits.txt'
+filename = ('/Users/david.wells/Desktop/pythonCrashCourse/chapter_10/pi_million_digits.txt')
 
 with open(filename) as file_object:
     lines = file_object.readlines()
-
+    
 pi_string = ''
-for line in lines:
+for line in lines: 
     pi_string += line.strip()
     
-print(f"{pi_string[:52]}...")
-print(len(pi_string))
+# print(f"{pi_string[:52]}...")
+# print(len(pi_string))
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
